68762772428aeeb4486fb3b8e52cd68920e59544
Darina <darina23sm@mail.ru>
2022-08-22 22:41:46 +0300

    seminar6

---
+++ task40/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число C");
+int C = int.Parse(Console.ReadLine()!);
+
+if (A < B + C && B < A + C && C < A + B)
+{
+    Console.WriteLine("Такой треугольник может существовать");
+}
+else
+{
+    Console.WriteLine("Такой треугольник невозможен");
+}
---
+++ task42/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int size = (int)Math.Log2(number) + 1;
+int[] array = new int[size];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = number % 2;
+    number = number / 2;
+}
+
+Array.Reverse(array);
+
+Console.WriteLine(size);
+Console.WriteLine(string.Join(", ", array));
